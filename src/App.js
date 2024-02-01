8cc1bd38eae2326a2410792df4b2a84c672d0f54
Yılmaz Keskin <104618545+Yamikar@users.noreply.github.com>
2024-02-01 19:06:20 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   
+    <div className='container'>
+      <div className='menu'></div>
+    </div>
+    </div>
+    
+  );
+}
+
+export default App;
